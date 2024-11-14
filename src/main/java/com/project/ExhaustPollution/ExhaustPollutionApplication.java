82c06591af84dd3158ca275a80ebd6a6a3c51ca6
Aryaman Awasthi <aryaman.awasthi79@gmail.com>
2024-11-15 01:01:19 +0530

    feat: mongodb connected

--- src/main/java/com/project/ExhaustPollution/ExhaustPollutionApplication.java
+++ src/main/java/com/project/ExhaustPollution/ExhaustPollutionApplication.java
@@ -1,9 +1,15 @@
 package com.project.ExhaustPollution;
 
+import com.project.ExhaustPollution.repository.DataEntryRepo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
+
 public class ExhaustPollutionApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/project/ExhaustPollution/entity/DataSet.java
+++ src/main/java/com/project/ExhaustPollution/entity/DataSet.java
@@ -1,7 +1,6 @@
 package com.project.ExhaustPollution.entity;
 
 import lombok.Data;
-import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -11,7 +10,7 @@ import java.time.LocalDateTime;
 @Data
 public class DataSet {
     @Id
-    private ObjectId _id;
+    private String _id;
     private double co;
     private double co2;
     private LocalDateTime timeStamp;
