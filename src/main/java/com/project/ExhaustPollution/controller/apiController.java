024675448f7998f134ad288c4817fbafc2d7772b
Aryaman Awasthi <aryaman.awasthi79@gmail.com>
2024-11-17 09:45:33 +0530

    update: added http status code

--- src/main/java/com/project/ExhaustPollution/controller/apiController.java
+++ src/main/java/com/project/ExhaustPollution/controller/apiController.java
@@ -4,6 +4,7 @@ import com.project.ExhaustPollution.entity.DataSet;
 import com.project.ExhaustPollution.service.DataSetEntryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
@@ -16,18 +17,31 @@ public class apiController {
     private DataSetEntryService dataSetEntryService;
 
     @GetMapping
-    public List<DataSet> getExhaustData() {
-        return dataSetEntryService.getAll();
+    public ResponseEntity<?> getExhaustData() {
+        try {
+            return new ResponseEntity<>(dataSetEntryService.getAll(), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @PostMapping
-    public void createDataSet(@RequestBody DataSet dataSet) {
-        dataSet.setTimeStamp(LocalDateTime.now());
-        dataSetEntryService.saveEntry(dataSet);
+    public ResponseEntity<Boolean> createDataSet(@RequestBody DataSet dataSet) {
+        try {
+            dataSet.setTimeStamp(LocalDateTime.now());
+            dataSetEntryService.saveEntry(dataSet);
+            return new ResponseEntity<>(true, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(false, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @GetMapping("/length")
-    public int getLength() {
-        return dataSetEntryService.getAll().size();
+    public ResponseEntity<?> getLength() {
+        try {
+            return new ResponseEntity<>(dataSetEntryService.getAll().size(), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
